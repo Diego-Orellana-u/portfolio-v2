8515e8825737cef3a004fb1943575863c4d00945
Diego Orellana <diego.orellana.git@gmail.com>
2024-04-02 21:07:34 -0300

    finished all responsive breakpoints in workflow section

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,82 +1,49 @@
+import { useId } from 'react';
+import WorkflowPoint from '../ui/WorkflowPoint';
+
+const workflowArr = [
+  ['01.', 'Recopilation of ideas and requirements'],
+  ['02.', 'Recopilation of ideas and requirements'],
+  ['03.', 'Recopilation of ideas and requirements'],
+  ['04.', 'Recopilation of ideas and requirements'],
+  ['05.', 'Recopilation of ideas and requirements'],
+  ['06.', 'Recopilation of ideas and requirements'],
+];
+
 export default function Workflow() {
+  const pointId = useId();
   return (
-    <section className="bg-black-bg py-8 px-[1.4rem] min-[615px]:py-16">
-      <h2 className="text-heading-3 font-semibold text-white-body-font pb-[2rem]">
+    <section className="bg-black-bg py-8 px-[1.4rem] min-[1024px]:px-[6rem] min-[615px]:py-16">
+      <h2 className="text-heading-3 font-semibold text-white-body-font pb-[2rem] min-[1024px]:text-[4rem] min-[1536px]:text-[5rem] s">
         My Workflow
       </h2>
-      <div className="flex justify-between min-[580px]:h-[400px] min-[698px]:h-[550px]">
-        <div className="flex">
-          <div className="w-5 mr-6 h-full min-[425px]:mr-12 min-[615px]:mr-16 min-[425px]:py-10">
+      <div className="flex justify-between min-[580px]:min-h-[400px] min-[698px]:min-h-[550px]">
+        <div className="flex w-[80%] min-[1220px]:w-[50%] justify-evenly">
+          <div className="w-5 mr-6 h-full min-[425px]:mr-12 min-[615px]:mr-16 min-[425px]:py-14">
             <div className="w-18px h-full radial"></div>
           </div>
           <div className="w-5 mr-2 h-full min-[390px]:mr-14 min-[425px]:mr-12 min-[615px]:mr-16">
             <div className="w-18px h-full radial"></div>
           </div>
-          <div className="w-5 mr-2 h-full min-[390px]:mr-14 min-[570px]:mr-12 max-[425px]:hidden min-[615px]:mr-16 min-[425px]:py-10">
+          <div className="w-5 mr-2 h-full min-[390px]:mr-14 min-[570px]:mr-12 max-[425px]:hidden min-[615px]:mr-16 min-[425px]:py-14">
             <div className="w-18px h-full radial"></div>
           </div>
           <div className="w-5 mr-2 h-full min-[390px]:mr-14 min-[570px]:mr-12 max-[615px]:hidden min-[615px]:mr-16">
             <div className="w-18px h-full radial"></div>
           </div>
-          <div className="w-5 mr-2 h-full min-[390px]:mr-14 min-[570px]:mr-12 max-[615px]:hidden min-[425px]:py-10">
+          <div className="w-5 mr-2 h-full min-[390px]:mr-14 min-[570px]:mr-12 max-[615px]:hidden min-[425px]:py-14">
             <div className="w-18px h-full radial"></div>
           </div>
         </div>
 
-        <div className="text-white-body-font flex flex-col gap-4 justify-between ">
-          <div className="flex items-start">
-            <span className="font-bold text-[1.3rem] text-big-nums tracking-[0.12em] w-[34px]">
-              01.
-            </span>
-            <p className="text-body-1 font-light pt-[3px] pl-3">
-              Recopilation of ideas and requirements
-            </p>
-          </div>
-
-          <div className="flex items-start">
-            <span className="font-bold text-[1.3rem] text-big-nums w-[34px]">
-              02.
-            </span>
-            <p className="text-body-1 font-light pt-[3px] pl-3">
-              Recopilation of ideas and requirements
-            </p>
-          </div>
-
-          <div className="flex items-start">
-            <span className="font-bold text-[1.3rem] text-big-nums w-[34px]">
-              03.
-            </span>
-            <p className="text-body-1 font-light pt-[3px] pl-3">
-              Recopilation of ideas and requirements
-            </p>
-          </div>
-
-          <div className="flex items-start">
-            <span className="font-bold text-[1.3rem] text-big-nums w-[34px]">
-              04.
-            </span>
-            <p className="text-body-1 font-light pt-[3px] pl-3">
-              Recopilation of ideas and requirements
-            </p>
-          </div>
-
-          <div className="flex items-start">
-            <span className="font-bold text-[1.3rem] text-big-nums w-[34px]">
-              05.
-            </span>
-            <p className="text-body-1 font-light pt-[3px] pl-3">
-              Recopilation of ideas and requirements
-            </p>
-          </div>
-
-          <div className="flex items-start">
-            <span className="font-bold text-[1.3rem] text-big-nums w-[34px]">
-              06.
-            </span>
-            <p className="text-body-1 font-light pt-[3px] pl-3">
-              Recopilation of ideas and requirements
-            </p>
-          </div>
+        <div className="flex flex-col gap-4 justify-between ">
+          {workflowArr.map((point, idx) => (
+            <WorkflowPoint
+              key={`${pointId} - ${idx}`}
+              number={point[0]}
+              point={point[1]}
+            />
+          ))}
         </div>
       </div>
     </section>
---
+++ src/ui/WorkflowPoint.jsx
@@ -0,0 +1,16 @@
+export default function WorkflowPoint({ number, point }) {
+  return (
+    <div className="flex items-start text-white-body-font">
+      <span
+        className={`font-bold text-[1.3rem] min-[740px]:text-[1.5rem] min-[880px]:text-[1.8rem] ${
+          number === '01.' && 'tracking-[0.14rem]'
+        } text-big-nums w-[34px] min-[740px]:pt-[2px] min-[880px]:pt-[4px]  min-[880px]:pr-[3rem]`}
+      >
+        {number}
+      </span>
+      <p className="text-body-1 font-light pt-[3px] pl-3 min-[740px]:text-[1.5rem] min-[880px]:text-[2rem]">
+        {point}
+      </p>
+    </div>
+  );
+}
