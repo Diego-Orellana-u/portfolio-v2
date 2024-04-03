e758bbeda392827ef2d08ffacecc2862c0bbda70
Diego Orellana <diego.orellana.git@gmail.com>
2024-04-03 15:01:48 -0300

    improved layout of hero, changing from relative/absolute positions to flexbox

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="h-[calc(100vh-135px)] min-[1024px]:h-[calc(100vh-80px)] relative"
+      className="h-[calc(100vh-135px)] min-[1024px]:h-[calc(100vh-80px)]"
     >
-      <div className="text-[58px] min-[550px]:text-[108px] min-[1024px]:text-[128px] font-bold relative top-[15vh] min-[375px]:top-[4vh] min-[425px]:top-[7vh] px-[1.4rem] min-[550px]:top-[0] min-[1024px]:top-[5vh]">
-        <div className="flex flex-col items-center">
+      <div className="flex min-h-full justify-between flex-col text-[58px] min-[550px]:text-[108px] min-[1024px]:text-[128px] font-bold px-[1.4rem]">
+        <div className="flex flex-col items-center pt-16 min-[550px]:pt-[0] min-[1024px]:pt-[13vh] min-[1424px]:pt-[7vh]">
           <div>
             <span className="font-general text-transparent font-outline-3">
               HI
@@ -19,7 +19,7 @@ export default function Hero() {
             <img
               src={logo}
               alt="hero logo of Factorial"
-              className="w-[260px] h-[88px] min-[550px]:w-[430px] min-[550px]:h-[143px] min-[1024px]:w-[736px] min-[1024px]:h-[213px]  ml-[1rem] min-[345px]:ml-[3rem]"
+              className="w-[260px] h-[88px] min-[550px]:w-[430px] min-[550px]:h-[143px] min-[1024px]:w-[736px] min-[1024px]:h-[213px] ml-[1rem] min-[345px]:ml-[3rem]"
             />
             <h1 className="text-body-1 font-normal text-center mt-[5px]">
               SOFTWARE DEVELOPMENT \\
@@ -27,22 +27,24 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className="mt-[10vh] min-[375px]:mt-[20vh] min-[550px]:mt-[2.5rem] min-[550px]:text-end min-[1024px]:mt-[1.5rem]">
-          <span className="hero-span">AVAILABLE FOR WORK //</span>
-          <span className="hero-span underline">+569 4266 8187</span>
-          <span className="hero-span underline">
-            DIEGO.ORELLANA.GIT@GMAIL.COM
+        <div className="pb-[5vh] min-[550px]:text-end">
+          <div className="pb-10">
+            <span className="hero-span">AVAILABLE FOR WORK //</span>
+            <span className="hero-span underline">+569 4266 8187</span>
+            <span className="hero-span underline">
+              DIEGO.ORELLANA.GIT@GMAIL.COM
+            </span>
+          </div>
+          <span className="block text-body-1 font-normal text-center">
+            Scroll down to see more
           </span>
         </div>
-        <span className="block text-body-1 font-normal text-center mt-8 min-[1024px]:mt-[0]">
-          Scroll down to see more
-        </span>
       </div>
-      <div className="absolute w-full top-[-9vh] min-[550px]:top-[-1vh] overflow-hidden">
+      <div className="relative bottom-[30rem] min-[900px]:bottom-[32rem] min-[1024px]:bottom-[42rem] min-[1424px]:bottom-[43rem] overflow-hidden">
         <img
           src={pattern}
           alt="vector pattern of the hero"
-          className="w-[270%] min-[750px]:w-[124%] min-[1024px]:w-[100%] max-w-none translate-x-[-45%] min-[768px]:translate-x-[-10%] translate-y-[15%] min-[425px]:translate-y-[0%] min-[1024px]:translate-x-[0%]"
+          className="w-[175%] min-[540px]:w-[135%] min-[750px]:w-[124%] min-[1024px]:w-[100%] max-w-none"
         />
       </div>
     </section>
